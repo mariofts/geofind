6b473f74e581201f1e0185b7dedf716ce5c50043
Mario Amaral <mario.fts@gmail.com>
2011-08-25 18:17:16 -0300

    Alterando o script para embarcar qualquer gem

--- jruby-gem-build.sh
+++ jruby-gem-build.sh
@@ -1,35 +1,54 @@
 #!/bin/bash
 
-#JRUBY_JAR=$1
-#VRAPTOR_JAR="vraptor-scaffold-complete.jar"
+GEM_NAME=$1
+JRUBY_JAR=
+
+arquivo="jruby-complete*.jar"
+
+if $(test -f $arquivo); then
+	 for i in $(ls $arquivo); 
+		 do JRUBY_JAR=$i;
+	 done
+else
+	echo " jruby não existe."
+	exit -1
+fi
+
+echo "backup jruby jar..."
+
+cp $JRUBY_JAR "$JRUBY_JAR.bak"
 
 echo "installing gems..."
 
-#GEM_TEMP="gems-temp"
-#java -jar $JRUBY_JAR -S gem install -i ./$GEM_TEMP vraptor-scaffold
+GEM_TEMP="gems-temp"
+java -jar $JRUBY_JAR -S gem install -i ./$GEM_TEMP $GEM_NAME
 
 echo 'building file structure...'
-#META_TEMP="meta-temp"
+META_TEMP="meta-temp"
 
-#BIN_PATH="$META_TEMP/META-INF/jruby.home"
-#GEM_PATH="$META_TEMP/META-INF/jruby.home/lib/ruby/gems/1.8"
+BIN_PATH="$META_TEMP/META-INF/jruby.home"
+GEM_PATH="$META_TEMP/META-INF/jruby.home/lib/ruby/gems/1.8"
 
-#mkdir -p $BIN_PATH
-#mkdir -p $GEM_PATH
+mkdir -p $BIN_PATH
+mkdir -p $GEM_PATH
 
-#cp -r $GEM_TEMP/bin $BIN_PATH 
-#cp -r $GEM_TEMP/gems $GEM_PATH
-#cp -r $GEM_TEMP/specifications $GEM_PATH
+cp -r $GEM_TEMP/bin $BIN_PATH 
+cp -r $GEM_TEMP/gems $GEM_PATH
+cp -r $GEM_TEMP/specifications $GEM_PATH
 
 echo "building jar..."
-#cp $JRUBY_JAR $VRAPTOR_JAR
 
-#jar uf $VRAPTOR_JAR -C $META_TEMP .
+JAR_NAME="$GEM_NAME-gems-complete.jar"
 
+cp $JRUBY_JAR $JAR_NAME
+
+jar uf $JAR_NAME -C $META_TEMP .
 
 echo 'removing temp gem files...'
-#rm -r $GEM_TEMP
-#rm -r $META_TEMP
+rm -r $GEM_TEMP
+rm -r $META_TEMP
 
+echo "restoring jruby-jar"
 
+mv "$JRUBY_JAR.bak" $JRUBY_JAR 
 
